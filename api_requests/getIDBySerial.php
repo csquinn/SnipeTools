a4fd05042cfe91a1e0e7ef880e4389cc3ce57c9e
csquinn <129312789+csquinn@users.noreply.github.com>
2025-06-12 10:18:48 -0400

    Update getIDBySerial.php

--- api_requests/getIDBySerial.php
+++ api_requests/getIDBySerial.php
@@ -31,7 +31,7 @@ try {
 	if ($response->getStatusCode() == 200) {
 		//convert json response into array
 		$assetJsonArray = json_decode($response->getBody(), true);
-		print_r($assetJsonArray);
+		print_r($assetJsonArray["rows"]);
 		//if asset doesn't exist in inventory
 		if(array_key_exists('status',$assetJsonArray)) {
 			echo "Asset not found";
