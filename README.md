ab61a0b73dc5bfc8b2f05d2468d738cda1b21321
csquinn <129312789+csquinn@users.noreply.github.com>
2025-06-12 10:11:06 -0400

    Testing getIDBySerial

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # SnipeTools
 SnipeTools is a web app that contains various tools for the Snipe IT Asset Management platform. These tools were designed specifically for my internship's Snipe IT implementation.
+
+IMPORTANT! If you get a PHP error about a self-signed certificate with cURL, follow this guide https://php.watch/articles/php-curl-windows-cainfo-fix
+the cacert.pm file is a file that contains many root CAs. Without it, cURL has no source to validate any certs with and will throw an error
--- api_requests/getIDBySerial.php
+++ api_requests/getIDBySerial.php
@@ -9,20 +9,42 @@ $api_key = str_replace(array("\r", "\n"), '', $api_key);
 $snipe_url = file_get_contents("../snipe_url.txt");
 $snipe_url = str_replace(array("\r", "\n"), '', $snipe_url);
 
-
+//assign variables gotten from request
 if ($_SERVER['REQUEST_METHOD'] === 'GET') {
-	$source = $_GET['source'];
-	$serial = $_GET['serial'];
+	$source = $_GET['source']; //which php file sent us here, used to decide which api call to make
+	$serial = $_GET['serial']; //serial number of asset
 }
-$client = new \GuzzleHttp\Client();
 
-$response = $client->request('GET', $snipe_url.'/api/v1/hardware/byserial/'.$serial.'?deleted=false', [
-	'headers' => [
-		'Authorization' => 'Bearer '.$api_key,
-		'accept' => 'application/json',
-	],
-]);
+//get request api call
+try {
+	$client = new \GuzzleHttp\Client();
 
-echo $response->getBody();
+	//utilizes $api_key and $snipe_url
+	$response = $client->request('GET', $snipe_url.'/api/v1/hardware/byserial/'.$serial.'?	deleted=false', [
+		'headers' => [
+			'Authorization' => 'Bearer '.$api_key,
+			'accept' => 'application/json',
+		],
+	]);
 
+	//if asset is found or doesn't exist, basically if there's no internal/api/server errors
+	if ($response->getStatusCode() == 200) {
+		//convert json response into array
+		$assetJsonArray = json_decode($response->getBody(), true);
+
+		//if asset doesn't exist in inventory
+		if($assetJsonArray["status"] == "error") {
+			echo "Asset not found";
+			//in SnipeTools, an id of -2 means the asset was not found in inventory
+			$id = -2;
+		} else {
+			$id = $assetJsonArrya["id"];
+			echo $id;
+		}
+	}
+} catch (\GuzzleHttp\Exception\RequestException $e) {
+	echo 'API Request Error: ' . $e->getMessage();
+} catch (\Exception $e {
+	echo 'General Error: ' . $e->getMessage();
+}
 ?>
