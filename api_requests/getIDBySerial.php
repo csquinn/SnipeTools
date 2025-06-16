57e6c9157472766d75ac1970dc823237bd4d0315
csquinn <129312789+csquinn@users.noreply.github.com>
2025-06-16 13:09:39 -0400

    Update getIDBySerial.php

--- api_requests/getIDBySerial.php
+++ api_requests/getIDBySerial.php
@@ -45,7 +45,7 @@ if (isset($_GET['GAdmin'])) {
 
 		//make api call with the directory object
 		$results = $service->chromeosdevices->listChromeosdevices($google_customer_id, $optParams); 	
-		echo $results['chromeosdevices'][0]['status'];	
+		echo $results->getChromeosdevices()[0]->getStatus();	
 	} catch (Google_Service_Exception $e) {
 		echo 'API Request Error: ' . $e->getMessage();
 	} catch (Google_Exception $e) {
