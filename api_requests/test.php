f917211b0cc66bcb0113d61a4f49786d8a18d1c9
csquinn <129312789+csquinn@users.noreply.github.com>
2025-06-18 15:29:22 -0400

    Create test.php

---
+++ api_requests/test.php
@@ -0,0 +1,18 @@
+<?php
+$retag = "on";
+$serial = "1";
+$assetTag" = "2";
+$status = "4";
+$currentStatus="5";
+$modelID = "6";
+$location= "7";
+$remName = "on";
+echo '{"'
+		.((isset($retag) and $retag=="on")?('"asset_tag": "'.$serial.'",'):('"asset_tag": "'.$assetTag.'",'))	//asset_tag
+		.((isset($status))?('"status_id": '.$status.','):('"status_id": '.$currentStatus.','))	//status_id
+		.'"model_id": '.$modelID.','	//model_id
+		.((isset($location) and $location != "lai")?('"rtd_location_id": '.$location.','):(''))	//rtd_location_id
+		.((isset($remName) and $remName=="on")?('"name": ""'):(''))	//name
+		.'"}';
+
+?>
