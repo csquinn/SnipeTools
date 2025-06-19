ab27d4acadf6fd7946bdf4d33b7923b5c53ed890
csquinn <129312789+csquinn@users.noreply.github.com>
2025-06-19 10:02:34 -0400

    Update validateAPI.php

--- api_requests/validateAPI.php
+++ api_requests/validateAPI.php
@@ -81,6 +81,14 @@ try {
 
 	//api request copied from snipeIT
 	//important note: I did not have to list every single asset field in this request, just the ones I wanted to update. Anything not mentioned is not touched
+	echo '{'
+		.((isset($retag) and $retag=="on")?('"asset_tag": "'.$serial.'"'):('"asset_tag": "'.$assetTag.'"'))	//asset_tag
+		.((isset($status))?(', "status_id": '.$status):(', "status_id": '.$currentStatus))	//status_id
+		.', "model_id": '.$modelID	//model_id
+		.((isset($location) and $location != "lai")?(', "rtd_location_id": '.$location):(''))	//rtd_location_id
+		.((isset($remName) and $remName=="on")?(', "name": null'):(''))	//name
+		.'}';
+	echo $currentStatus;
 	$response = $client->request('PUT', $snipe_url.'/api/v1/hardware/'.$id, [
 		'body' =>'{'
 		.((isset($retag) and $retag=="on")?('"asset_tag": "'.$serial.'"'):('"asset_tag": "'.$assetTag.'"'))	//asset_tag
