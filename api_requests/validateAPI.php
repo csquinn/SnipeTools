4a89ba72feff4bb6ef8754dac0f251faf4a3d3fd
csquinn <129312789+csquinn@users.noreply.github.com>
2025-06-19 08:40:37 -0400

    Update validateAPI.php

--- api_requests/validateAPI.php
+++ api_requests/validateAPI.php
@@ -133,9 +133,9 @@ try {
 		'body' =>'{'
 		.((isset($retag) and $retag=="on")?('"asset_tag": "'.$serial.'",'):('"asset_tag": "'.$assetTag.'",'))	//asset_tag
 		.((isset($status))?('"status_id": '.$status.','):('"status_id": '.$currentStatus.','))	//status_id
-		.'"model_id": '.$modelID.','	//model_id
-		.((isset($location) and $location != "lai")?('"rtd_location_id": '.$location.','):(''))	//rtd_location_id
-		.((isset($remName) and $remName=="on")?('"name": null'):(''))	//name
+		.'"model_id": '.$modelID	//model_id
+		.((isset($location) and $location != "lai")?(', "rtd_location_id": '.$location.','):(''))	//rtd_location_id
+		.((isset($remName) and $remName=="on")?(', "name": null'):(''))	//name
 		.'}',
 		'headers' => [
 			'Authorization' => 'Bearer ' . $api_key,
