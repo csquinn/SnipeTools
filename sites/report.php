16d51cdffce9225848612f49c3b7a30376b6d6f4
csquinn <129312789+csquinn@users.noreply.github.com>
2025-06-23 13:15:48 -0400

    Beginning report.php

---
+++ sites/report.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php
+
+//Create mysqli connection
+$mysqli = new mysqli("localhost","snipe_user","notrealpassword","snipeit");
+
+// Check connection
+if ($mysqli -> connect_errno) {
+  echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
+  exit();
+}
+
+?>
+
+<style> 
+body {background-color: #337ab7; color: white;} 
+a:link{color:white;}
+a:visited{color:white;}
+a:hover{color:white;}
+a:focus{color:white;}
+a:active{color:white;}
+</style>
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Inventory Health Report | SnipeTools</title>
+</head>
+<body>
+	<div style="margin: 0; position: absolute; top: 40%; left: 50%; -ms-transform: translate(-50%, -50%); transform: translate(-50%, -50%); text-align: center;">
+	<h1>Inventory Health Report</h1>
+	<h3>This site queries SnipeIT and returns assets that are believed to be errors. Each different section can be expanded below</h3>
+	<h3><b>*Please note: This report does not actually modify SnipeIT in any way</b></h3>
+	<br>
+	
+	<php?
+
+	$sql = "select * from assets inner join users on assets.assigned_to = users.id where users.username like "%99%" and length(users.username) != 9 and assets.deleted_at is null;";
+
+	$result = $mysqli -> query($sql);
+
+	// Associative array
+	$row = $result -> fetch_assoc();
+	echo $row['assets.asset_tag'];
+	echo $row['users.username'];
+
+	// Free result set
+	$result -> free_result();
+
+	$mysqli -> close();
+	
+	?>
+	</div>
+</body>
+</html>
--- update.bat
+++
@@ -1,3 +0,0 @@
-cd C:\wamp64\www\snipe-it
-git pull
-php upgrade.php --no-interactive
