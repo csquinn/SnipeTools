8932f9dac9c73488836769d6cc986176e3f96e62
csquinn <129312789+csquinn@users.noreply.github.com>
2025-06-24 12:53:33 -0400

    Update modifyExclusions.php

--- sites/modifyExclusions.php
+++ sites/modifyExclusions.php
@@ -1,18 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<?php
-//create 
-$exclusionList = file_get_contents("../exclusions.txt");
-//assign variable to write to file and prefill textarea
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-	$exclusionList = $_POST['exclusions'];
-	//write input to file
-	file_put_contents("../exclusions.txt", $exclusionList);
-}
-
-?>
-
 <style> 
 body {background-color: #337ab7; color: white;} 
 a:link{color:white;}
@@ -21,6 +9,26 @@ a:hover{color:white;}
 a:focus{color:white;}
 a:active{color:white;}
 </style>
+
+<?php
+//initialize success message
+$successMessage = "";
+
+//fill exclusionList with file contents of txt file
+$exclusionList = file_get_contents("../exclusions.txt");
+
+//reassign exclusionList if it's being updated
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+	$exclusionList = $_POST['exclusions'];
+	//write input to file
+	file_put_contents("../exclusions.txt", $exclusionList);
+	//pretty color
+	echo "body {background-color: green; color: black;};"
+	$successMessage = "Successfully updated the Exclusion List";
+}
+
+?>
+
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -28,15 +36,17 @@ a:active{color:white;}
 </head>
 <body>
 	<div style="text-align: center;">
-	<h2>Enter any Asset Tags, Serial Numbers, and Usernames (99#s) you would like to exclude from the Inventory Health Report</h2>
+	<h2>Enter any Asset Tags, Serial Numbers, and Usernames (99#s) you would</h2>
+	<h2>like to exclude from the Inventory Health Report</h2>
 	<h3>Enter each value as a new line with <b>NO</b> commas</h3>
 	<h3>It doesn't matter if you separate tags, serials, or usernames, enter them in any order</h3>
 	<form action="modifyExclusions.php" method="POST">
-		<textarea id="exclusions" name="exclusions" rows="35" cols="35"><?php echo $exclusionList; ?></textarea>
+		<textarea id="exclusions" name="exclusions" rows="25" cols="35"><?php echo $exclusionList; ?></textarea>
 		<br>
 		<input type="submit" value="Submit">
 	</form>
-
+	<h2><?php echo $successMessage; ?></h2>
+	<a href = "report.php">Back to Inventory Health Report</a>
 	</div>
 </body>
 </html>
