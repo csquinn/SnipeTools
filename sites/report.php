1cef96e57d6838bf1d75b4ddc4a44e7ff86127bb
ER-Walker <erwalker816@icloud.com>
2025-06-30 07:47:07 -0400

    Functional
    Now with functions!

--- sites/report.php
+++ sites/report.php
@@ -147,19 +147,7 @@ table {text-align: center; margin: auto;}
 	<?php
 	//Assets with 7 character or less asset tags
 	$sql = 'select * from assets where length(asset_tag) < 7 and deleted_at is null and asset_tag not in (select name from tempExclusions) and serial not in (select name from tempExclusions);';
-	$result = $mysqli -> query($sql);
-	echo "<details>";
-	echo "<summary>Assets with 7 Character or Less Asset Tags (". $result->num_rows .")</summary>";
-	// Associative array
-	echo "<table border='1'>";
-	echo "<tr><td>Asset Tag</td><td>Serial</td><td>Link</td></tr>";
-	while($row = $result -> fetch_assoc()){
-		echo "<tr><td>". $row['asset_tag'] ."</td><td>". $row['serial'] ."</td><td><a href='" . $snipe_url . "/hardware?page=1&size=20&search=" . $row['serial'] . "'>Link</a></td></tr>";
-	}
-	echo"</table>";
-	echo "</details>";
-	// Free result set
-	$result -> free_result();
+	getTagSerial($sql, $mysqli, $snipe_url, "Assets with 7 Character or Less Asset Tags");
 	?>
 	<br>
 
