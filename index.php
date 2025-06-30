b9c327ee60b1672eaf2737936ff627ee61669bc5
ER-Walker <erwalker816@icloud.com>
2025-06-30 10:42:09 -0400

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="stylesheet" href="/styles/indexStyles.css">
+	<link rel="stylesheet" href="styles/indexStyle.css">
 	<title>SnipeTools</title>
 </head>
 <body>
