76cf24520902d88c5978667e54904a33cba6fa87
ER-Walker <erwalker816@icloud.com>
2025-06-30 07:45:08 -0400

    Update report.php

--- sites/report.php
+++ sites/report.php
@@ -95,21 +95,6 @@ table {text-align: center; margin: auto;}
 	<?php
 	//All assets where test is in asset tag but not testing
 	$sql = 'select * from assets where asset_tag like "%test%" and asset_tag not like "%testing%" and deleted_at is null and asset_tag not in (select name from tempExclusions) and serial not in (select name from tempExclusions);';
-	/*
-	$result = $mysqli -> query($sql);
-	echo "<details>";
-	echo "<summary> (". $result->num_rows .")</summary>";
-	// Associative array
-	echo "<table border='1'>";
-	echo "<tr><td>Asset Tag</td><td>Serial</td><td>Link</td></tr>";
-	while($row = $result -> fetch_assoc()){
-		echo "<tr><td>". $row['asset_tag'] ."</td><td>". $row['serial'] ."</td><td><a href='" . $snipe_url . "/hardware?page=1&size=20&search=" . $row['serial'] . "'>Link</a></td></tr>";
-	}
-	echo"</table>";
-	echo "</details>";
-	// Free result set
-	$result -> free_result();
-	*/
 	getTagSerial($sql, $mysqli, $snipe_url, "Assets Believed to be SnipeIT Tests that were Never Deleted");
 	?>
 	<br>
@@ -155,19 +140,7 @@ table {text-align: center; margin: auto;}
 	<?php
 	//chromebooks with spaces in their asset tags and asset tag != serial
 	$sql = 'select * from assets inner join models on assets.model_id = models.id inner join categories on models.category_id = categories.id where categories.name = "Chromebook" and assets.asset_tag not like "% %" and assets.asset_tag != assets.serial and assets.deleted_at is null and asset_tag not in (select name from tempExclusions) and serial not in (select name from tempExclusions);';
-	$result = $mysqli -> query($sql);
-	echo "<details>";
-	echo "<summary>Chromebooks without a space in their Asset Tags (". $result->num_rows .")</summary>";
-	// Associative array
-	echo "<table border='1'>";
-	echo "<tr><td>Asset Tag</td><td>Serial</td><td>Link</td></tr>";
-	while($row = $result -> fetch_assoc()){
-		echo "<tr><td>". $row['asset_tag'] ."</td><td>". $row['serial'] ."</td><td><a href='" . $snipe_url . "/hardware?page=1&size=20&search=" . $row['serial'] . "'>Link</a></td></tr>";
-	}
-	echo"</table>";
-	echo "</details>";
-	// Free result set
-	$result -> free_result();
+	getTagSerial($sql, $mysqli, $snipe_url, "Chromebooks without a space in their Asset Tags");
 	?>
 	<br>
 
