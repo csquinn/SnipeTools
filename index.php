ee3c8f2bd6d86e754ab63e1a042860c2639f01b2
ER-Walker <erwalker816@icloud.com>
2025-06-30 10:56:50 -0400

    styles

--- index.php
+++ index.php
@@ -7,22 +7,24 @@
 	<title>SnipeTools</title>
 </head>
 <body>
-	<div id = "title">
-		<h1>SnipeTools</h1>
-	</div>
+	<div class = "wrapper">
+		<div id = "title">
+			<h1>SnipeTools</h1>
+		</div>
 
-	<?php echo ((time()-filemtime("../snipe-it/.github") > 30 * 24 * 3600)?("<h3 class = 'alert'>SnipeIT hasn't been updated in over a month. Please remote into<br>this server and run the SnipeIT update script on the Desktop.</h3>"):("")); ?>
-	
-	<div id = "sites">
-		<a href="sites/validate.php">Asset Validation</a>
-		<a href="sites/office.php">Return to Office</a>
-		<a href="sites/deprovision.php">Deprovisioning</a>
-		<a href="sites/report.php">Inventory Health Report</a>
-		<a href="sites/plug.php">TEMPORARY</a>
-	</div>	
-	
-	<div id = "cred">
-		<a href="credits.html">Credits</a>
+		<?php echo ((time()-filemtime("../snipe-it/.github") > 30 * 24 * 3600)?("<h3 class = 'alert'>SnipeIT hasn't been updated in over a month. Please remote into<br>this server and run the SnipeIT update script on the Desktop.</h3>"):("")); ?>
+		
+		<div id = "sites">
+			<a href="sites/validate.php">Asset Validation</a>
+			<a href="sites/office.php">Return to Office</a>
+			<a href="sites/deprovision.php">Deprovisioning</a>
+			<a href="sites/report.php">Inventory Health Report</a>
+			<a href="sites/plug.php">TEMPORARY</a>
+		</div>	
+		
+		<div id = "cred">
+			<a href="credits.html">Credits</a>
+		</div>
 	</div>
 </body>
 </html>
