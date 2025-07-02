d686239453bb2c85208e43b132cca3d1047fcbc8
ER-Walker <erwalker816@icloud.com>
2025-07-02 07:46:52 -0400

    Button
    Clicky Clicky

--- index.php
+++ index.php
@@ -20,11 +20,11 @@
 		<?php echo ((time()-filemtime("../snipe-it/.github") > 30 * 24 * 3600)?("<h3 class = 'alert'>SnipeIT hasn't been updated in over a month. Please remote into<br>this server and run the SnipeIT update script on the Desktop.</h3>"):("")); ?>
 		
 		<div id = "sites">
-			<button data-url ="sites/validate.php">Asset Validation</button>
-			<button data-url ="sites/office.php">Return to Office</button>
-			<button data-url ="sites/deprovision.php">Deprovisioning</button>
-			<button data-url = "sites/report.php">Inventory Health Report</button>
-			<button data-url = "sites/plug.php">TEMPORARY</button>
+			<button class = "button" data-url ="sites/validate.php">Asset Validation</button>
+			<button class = "button" data-url ="sites/office.php">Return to Office</button>
+			<button class = "button" data-url ="sites/deprovision.php">Deprovisioning</button>
+			<button class = "button" data-url = "sites/report.php">Inventory Health Report</button>
+			<button class = "button" data-url = "sites/plug.php">TEMPORARY</button>
 			<script type = "module" src = "scripts/buttons.js"></script>
 		</div>	
 		
