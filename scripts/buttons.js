0f9fa1bf452f3aebc6c74f535ac8d0f0f378007c
ER-Walker <erwalker816@icloud.com>
2025-07-02 11:12:29 -0400

    Update buttons.js

--- scripts/buttons.js
+++ scripts/buttons.js
@@ -1,16 +1,25 @@
 //search for event to occur within document
 document.addEventListener("DOMContentLoaded", function () {
-    console.log("Script loaded");
 
     //const that selects all button elements with URLs
     const buttons = document.querySelectorAll("button[data-url]");
+
 
     //listen for click on each button
     buttons.forEach(button => {
         button.addEventListener("click", function () {
             
+            //I am a little too stubborn to learn how to properly
+            //send data to the site and back to eliminate errors
+            //in the console log. The buttons work as is, but the
+            //errors may be present in console. - E
+
             //get URL attribute from the button clicked
             const targetUrl = button.getAttribute("data-url");
+            
+            //send response to console
+            sendResponse({success: true, data: "going to new page"});
+
             //if there is a URL present, change webpage to the URL
             if(targetUrl) {
                 window.location.href = targetUrl;
