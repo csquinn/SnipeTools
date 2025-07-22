45b61bb21f536321e4b3b7d5270d328418690c1c
csquinn <129312789+csquinn@users.noreply.github.com>
2025-07-22 08:08:18 -0400

    Update getIDBySerial.php

--- api_requests/getIDBySerial.php
+++ api_requests/getIDBySerial.php
@@ -89,7 +89,8 @@ try {
 			//write this scan to the badscans.txt file for logging purposes
 			file_put_contents("../logs/badscans.txt", $serial."\n", FILE_APPEND);
 
-			//extra header information is optionally provided if the source is validate.php to keep previously checked boxes checked 
+			//extra header information is optionally provided if the source is validate.php to keep previously checked boxes checked
+echo "why we here"; 
 			header("Location: ../sites/" . $source . ".php?SnipeRequestStatus=-1&serial=". $serial . 
 				(isset($_GET['GAdmin']) ? ("&GoogleRequestStatus") : "").
 				((isset($_GET['status']))?("&status=".$_GET['status']):("")).
