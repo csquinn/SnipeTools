b9381fdfa1c92f148478b65a0b43276a756d0775
csquinn <129312789+csquinn@users.noreply.github.com>
2025-08-05 17:00:06 -0400

    Update reportFunctions.php

--- sites/reportFunctions.php
+++ sites/reportFunctions.php
@@ -132,4 +132,41 @@
         // Free result set
         $result -> free_result();
     }
+
+/**
+ * Gathers asset info from 
+ * multiple SQL calls and
+ * compiles it into a report
+ * capturing every K-4 Chromebook
+ * in the district
+ * $rooms is a 2d array of every
+ * room to check as well as the amount
+ * of CBs supposed to be in the room
+*/
+function getK4Errors($rooms, $mysql_arg, $snipe_arg, $cat_arg){
+	
+	echo "<details>";
+	echo "<summary>".$cat_arg."</summary>";
+	echo "<table border='1'>";
+	foreach($rooms as $class){//iterate through each room
+		for($x = 0; $x < (int)$class[1], $x++){//iterate through each CB in each room
+			
+			//This is the MySQL statement that gets executed for each asset to generate the report
+			$mySQLCBS = "select * from assets where asset_tag = '".$class[0]."-".(($x < 10)?("0".$x):($x))."';";//ternary operator to add leading 0
+		    	$result = $mysql_arg -> query($mySQLCBS);
+			if($result->num_rows < 1){//didn't find it
+				echo "<tr><td id = 'tableElement'>". $class[0]."-".(($x < 10)?("0".$x):($x)) ."</td><td id = 'tableElement'>Not Found</td><td id = 'tableElement'><a href='" . $snipe_arg . "/hardware?page=1&size=20&search=" . $class[0]."-".(($x < 10)?("0".$x):($x)) . "' target = '_blank'>Link</a></td></tr>";
+			} else if($result->num_rows > 1){//found more than 1, should never happen as asset tags are unique
+				echo "<tr><td id = 'tableElement'>You should not be seeing this</td></tr>";
+			} else if($result->num_rows ===1){//found it
+				echo "<tr><td id = 'tableElement'>". $row['asset_tag'] ."</td><td id = 'tableElement'>Found</td><td id = 'tableElement'><a href='" . $snipe_arg . "/hardware?page=1&size=20&search=" . $row['serial'] . "' target = '_blank'>Link</a></td></tr>";
+			}
+			// Free result set
+      			  $result -> free_result();
+		}
+	}
+	echo"</table>";
+        echo "</details>";
+}
+
 ?>
