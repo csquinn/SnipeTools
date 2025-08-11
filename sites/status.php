a551df45a84e9feb3099b04d0eebd13152059f42
csquinn <129312789+csquinn@users.noreply.github.com>
2025-08-11 07:26:42 -0400

    Update status.php

--- sites/status.php
+++ sites/status.php
@@ -36,7 +36,7 @@ while (($line = fgets($handle)) !== false) {
 	if(!($line == "null" or $line == " " or $line == "" or $line == null)){
 		$temp = explode(',', $line);
 		if(substr($temp[4], 0, 2) == "99"){
-			echo substr($temp[4], 0, 2) == "99";
+			echo substr($temp[4], 0, 2) == "99" . "<br>";
 			$location = 0;
 			echo $line;
 			switch ($temp[0]) { //get locations of students from roster and match the id of snipe database
