6211079da52b9934357f14e4c11013c1d932eb49
csquinn <129312789+csquinn@users.noreply.github.com>
2026-01-05 13:08:21 -0500

    Rename SnipeTools to SummerTools in index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" href="styles/indexStyle.css">
-	<title>SnipeTools</title>
+	<title>SummerTools</title>
 </head>
 <body>
 	<div class = "imageWrapper">
@@ -13,7 +13,7 @@
 	
 	<div class = "wrapper">
 		<div id = "title">
-			<h1>SnipeTools</h1>
+			<h1>SummerTools</h1>
 		</div>
 		<hr>
 
